6370e52687e653c32e57cd875705edc8b5893267
Shabicode <monicaab.mtz95@gmail.com>
2025-01-05 01:08:26 -0600

    Singleton with delegates

--- PatternCS/Program.cs
+++ PatternCS/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("**** Singleton Pattern Demostration **** \n");
+﻿using System.Text.RegularExpressions;
+using Microsoft.VisualBasic;
+
+Console.WriteLine("**** Singleton Pattern Demostration **** \n");
 Console.WriteLine(Singleton.Instance.msg);
 Console.WriteLine(WeddingCake.MyInt);
 
@@ -13,3 +16,55 @@ if (firstInstance.Equals(secondInstance)){
 }
  Console.Read();
            
+
+Console.WriteLine(MaxScore("00"));
+//0 1 1 1 0 1
+int MaxScore(string s) {
+    int maxScore = 0;
+    for(int i = 0; i < s.Length - 1; i++ ) {
+        int leftScore = calculateScore('0', s[..(i+1)]);
+        int rightScore = calculateScore('1',s[(i+1)..]);
+        maxScore = Math.Max(leftScore, rightScore);
+    }
+    return maxScore;
+}
+
+int calculateScore(char number, string subString) {
+    int count = 0;
+    foreach (char c in subString) {
+        if (c == number) { count += 1; }
+    }
+    return count;
+}
+//words = ["aba","bcb","ece","aa","e"], queries = [[0,2],[1,4],[1,1]]
+string[] words =["aba","bcb","ece","aa","e"];
+int[][] queries = [[0,2],[1,4],[1,1]];
+
+//0,1 1,0, 2,1
+
+var result = vowelStrings(words, queries);
+Console.WriteLine(result);
+int[] vowelStrings(string[] words, int[][] queries) {
+    int[] result = new int[queries.Length];
+    int[] wordsWithVowel = arrayVowelWords(words);
+    int[] prefixSum = new int[wordsWithVowel.Length + 1];
+
+    for(int i = 0; i < wordsWithVowel.Length; i++){
+        prefixSum[i + 1] = prefixSum[i] + wordsWithVowel[i];
+    } 
+
+    for (int i = 0; i <= queries.Length - 1; i ++) {
+        result[i] = prefixSum[queries[i][1] + 1] - prefixSum[queries[i][0]];
+    }
+    return result;
+
+}
+///////////////////////////////////////////////////////////
+int[] arrayVowelWords (string[] words) {
+    int[] vowelWords = new int[words.Length];
+    var regex = new Regex("^[aeiouAEIOU](.*[aeiouAEIOU])?$");
+    for(int i = 0; i < words.Length; i ++) { 
+        vowelWords[i] = regex.IsMatch(words[i]) ? 1 : 0;
+    }
+    return vowelWords;
+}
--- PatternCS/obj/Debug/net8.0/PatternCS.AssemblyInfo.cs
+++ PatternCS/obj/Debug/net8.0/PatternCS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PatternCS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aba624a397dd3e45425f26ee78e884f427b6af47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69b37a44228a01e6ad61a3fbc0306a4b3837b938")]
 [assembly: System.Reflection.AssemblyProductAttribute("PatternCS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PatternCS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
